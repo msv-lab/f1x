a52c62cff24f0e05f37a8ea1c8fe39eabbbbfd20
Sergey Mechtaev <mechtaev@gmail.com>
2016-11-28 15:01:51 +0800

    fixing compilation and option parsing

--- tools/F1X.cpp
+++ tools/F1X.cpp
@@ -11,18 +11,18 @@ using namespace std;
 int main (int argc, char *argv[])
 {
   po::positional_options_description positional;
-  positional.add("source", 1);
+  positional.add("source", -1);
   
   // Declare supported options.
   po::options_description general("Usage: f1x PATH OPTIONS\n\nSupported options");
   general.add_options()
     ("files,f", po::value<string>()->multitoken()->value_name("RELPATH..."), "list of source files to repair")
     ("tests,t", po::value<string>()->multitoken()->value_name("ID..."), "list of test IDs")
-    ("test-timeout,T", po::value<string>()->value_name("MS"), "test execution timeout (default: none)")
+    ("test-timeout,T", po::value<int>()->value_name("MS"), "test execution timeout (default: none)")
     ("driver,d", po::value<string>()->value_name("PATH"), "test driver")
     ("build,b", po::value<string>()->value_name("CMD"), "build command (default: make -e)")
-    ("output,o", po::value<string>()->value_name("PATH"), "output patch file (default: $PWD/SRCDIR-DATE-TIME.patch)")
-    ("verbose,v", po::value<int>()->value_name("LEVEL")->implicit_value(1), "extended output")
+    ("output,o", po::value<string>()->value_name("PATH"), "output patch file (default: SRC-TIME.patch)")
+    ("verbose,v", po::value<int>()->value_name("LEVEL")->implicit_value(1), "produce extended output")
     ("help,h", "produce help message and exit")
     ("version", "print version and exit")
     ;
@@ -34,7 +34,8 @@ int main (int argc, char *argv[])
 
   po::options_description all("All options");
   all.add(general).add(hidden);
-  
+
+
   po::variables_map vm;
   po::store(po::command_line_parser(argc, argv).options(all).positional(positional).run(), vm);
   po::notify(vm);
