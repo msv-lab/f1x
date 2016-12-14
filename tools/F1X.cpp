7c6e78c23fedc4fbf4a79d9ecffcb7fb88d1c4c1
Sergey Mechtaev <mechtaev@gmail.com>
2016-12-15 07:38:54 +0800

    validate input

--- tools/F1X.cpp
+++ tools/F1X.cpp
@@ -19,11 +19,15 @@
 #include <iostream>
 #include <ctime>
 #include <sstream>
+#include <sys/stat.h>
 
 #include <boost/program_options.hpp>
+
 #include <boost/log/core.hpp>
 #include <boost/log/trivial.hpp>
 #include <boost/log/expressions.hpp>
+#include <boost/log/utility/setup/console.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
 
 #include "Config.h"
 #include "Repair.h"
@@ -34,6 +38,18 @@ namespace logging = boost::log;
 
 using std::vector;
 using std::string;
+
+
+bool isExecutable(const char *file)
+{
+  struct stat  st;
+
+  if (stat(file, &st) < 0)
+    return false;
+  if ((st.st_mode & S_IEXEC) != 0)
+    return true;
+  return false;
+}
 
 
 int main (int argc, char *argv[])
@@ -63,7 +79,6 @@ int main (int argc, char *argv[])
   po::options_description all("All options");
   all.add(general).add(hidden);
 
-
   po::variables_map vm;
   po::store(po::command_line_parser(argc, argv).options(all).positional(positional).run(), vm);
   po::notify(vm);
@@ -75,11 +90,14 @@ int main (int argc, char *argv[])
 
   if (vm.count("version")) {
     std::cout << "f1x " << F1X_VERSION_MAJOR <<
-               "." << F1X_VERSION_MINOR <<
-      "." << F1X_VERSION_PATCH << std::endl;
+                    "." << F1X_VERSION_MINOR <<
+                    "." << F1X_VERSION_PATCH << std::endl;
     return 1;
   }
 
+  logging::add_common_attributes();
+  logging::register_simple_formatter_factory< boost::log::trivial::severity_level, char >("Severity");
+  logging::add_console_log(std::cerr, logging::keywords::format = "[%TimeStamp%] [%Severity%]\t%Message%");
   if (vm.count("verbose")) {
     logging::core::get()->set_filter(logging::trivial::severity >= logging::trivial::debug);    
   } else {
@@ -87,42 +105,58 @@ int main (int argc, char *argv[])
   }
 
   if (!vm.count("source")) {
-    BOOST_LOG_TRIVIAL(error) << "source directory is not specified (use --help)\n";
+    BOOST_LOG_TRIVIAL(error) << "source directory is not specified (use --help)";
     return 1;
   }
   fs::path root(vm["source"].as<string>());
   root = fs::absolute(root);
-
-  uint testTimeout = 0;
-  if (vm.count("test-timeout")) {
-    testTimeout = vm["test-timeout"].as<uint>();
+  if (! (fs::exists(root) && fs::is_directory(root))) {
+    BOOST_LOG_TRIVIAL(error) << "source directory " << root.string() << " does not exist";
+    return 1;
   }
+
+  if (!vm.count("test-timeout")) {
+    BOOST_LOG_TRIVIAL(error) << "test execution timeout is not specified (use --help)";
+    return 1;
+  }
+  uint testTimeout = vm["test-timeout"].as<uint>();
 
   vector<fs::path> files;
   if (!vm.count("files")) {
-    BOOST_LOG_TRIVIAL(error) << "files are not specified (use --help)\n";
+    BOOST_LOG_TRIVIAL(error) << "files are not specified (use --help)";
     return 1;
   }
   vector<string> fileNames = vm["files"].as<vector<string>>();
   for(string &name : fileNames) {
     fs::path file(name);
-    // FIXME: check if file exists
+    fs::path fullPath = root / file;
+    if (! fs::exists(fullPath)) {
+      BOOST_LOG_TRIVIAL(error) << "source file " << fullPath.string() << " does not exist";
+      return 1;
+    }
     files.push_back(file);
   }
 
   if (!vm.count("tests")) {
-    BOOST_LOG_TRIVIAL(error) << "tests are not specified (use --help)\n";
+    BOOST_LOG_TRIVIAL(error) << "tests are not specified (use --help)";
     return 1;
   }
   vector<string> tests = vm["tests"].as<vector<string>>();
 
   if (!vm.count("driver")) {
-    BOOST_LOG_TRIVIAL(error) << "test driver is not specified (use --help)\n";
+    BOOST_LOG_TRIVIAL(error) << "test driver is not specified (use --help)";
     return 1;
   }
-  // FIXME: check if driver exists and executable
   fs::path driver(vm["driver"].as<string>());
   driver = fs::absolute(driver);
+  if (! fs::exists(driver)) {
+    BOOST_LOG_TRIVIAL(error) << "driver " << driver.string() << " does not exist";
+    return 1;
+  }
+  if (! isExecutable(driver.string().c_str())) {
+    BOOST_LOG_TRIVIAL(error) << "driver " << driver.string() << " is not executable";
+    return 1;
+  }
 
   string buildCmd = "make -e";
   if (vm.count("build")) {
@@ -147,10 +181,10 @@ int main (int argc, char *argv[])
   bool found = repair(root, files, tests, testTimeout, driver, buildCmd, output);
 
   if (found) {
-    std::cout << "SUCCESS" << std::endl;
+    BOOST_LOG_TRIVIAL(info) << "patch successfully generated: " << output.string();
     return 0;
   } else {
-    std::cout << "FAIL" << std::endl;
+    BOOST_LOG_TRIVIAL(info) << "failed to generated a patch";
     return 1;
   }
 }
