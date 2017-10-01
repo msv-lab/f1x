5d0493e684579382b952da0f288704f9c2e3b66a
Sergey Mechtaev <mechtaev@gmail.com>
2017-10-01 13:05:03 +0000

    multifile support

--- tests/runall.sh
+++ tests/runall.sh
@@ -97,6 +97,9 @@ get-cmd () {
         refine-condition)
             echo "f1x $dir --files program.c --driver $dir/test.sh --tests n1 p1 p2 --test-timeout 1000"
             ;;
+        multifile)
+            echo "f1x $dir --files program.c lib.c --driver $dir/test.sh --tests n1 p1 p2 --test-timeout 1000"
+            ;;
         *)
             exit 1
             ;;
